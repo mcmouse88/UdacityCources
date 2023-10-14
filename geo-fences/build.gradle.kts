e73add4f1b1bf0a1d053b4067b23169e5794b5f1
razdobudko.s <razdobudko.s@wb.ru>
2023-10-14 18:59:26 +0200

    Initial Lecture

--- geo-fences/build.gradle.kts
+++ geo-fences/build.gradle.kts
@@ -1,14 +1,15 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("kotlin-kapt")
 }
 
 android {
-    namespace = "com.mcmouse88.udacitycourses"
+    namespace = "com.mcmouse88.geo_fences"
     compileSdk = libs.versions.compileSdk.get().toInt()
 
     defaultConfig {
-        applicationId = "com.mcmouse88.udacitycourses"
+        applicationId = "com.mcmouse88.geo_fences"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
         versionCode = 1
@@ -33,6 +34,11 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+    buildFeatures {
+        dataBinding = true
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -44,4 +50,13 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.test.ext.junit)
     androidTestImplementation(libs.androidx.test.espresso.core)
+
+    implementation(libs.androidx.lifecycle.extensions)
+    implementation(libs.androidx.lifecycle.viewmodel.savedstate)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+
+    implementation(libs.google.android.gms.play.services.maps)
+    implementation(libs.google.android.gms.play.services.location)
 }
---
+++ geo-fences/src/main/java/com/mcmouse88/geo_fences/GeofenceBroadcastReceiver.kt
@@ -0,0 +1,14 @@
+package com.mcmouse88.geo_fences
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class GeofenceBroadcastReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        // TODO: Step 11 implement the onReceive method
+    }
+}
+
+private const val TAG = "GeofenceReceiver"
