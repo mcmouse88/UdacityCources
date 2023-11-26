c170616356e991b191c07c16347de27c5092dba5
razdobudko.s <razdobudko.s@wb.ru>
2023-11-26 02:49:41 +0100

    Code Checkpoint: viewModelScope and TestCoroutineDispatcher

--- basic-testing/src/test/java/com/mcmouse88/basic_testing/data/source/FakeTasksRepository.kt
+++ basic-testing/src/test/java/com/mcmouse88/basic_testing/data/source/FakeTasksRepository.kt
@@ -2,6 +2,7 @@ package com.mcmouse88.basic_testing.data.source
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.map
 import com.mcmouse88.basic_testing.data.Result
 import com.mcmouse88.basic_testing.data.Task
 import kotlinx.coroutines.runBlocking
@@ -12,59 +13,83 @@ class FakeTasksRepository : TasksRepository {
     private val observableTasks = MutableLiveData<Result<List<Task>>>()
 
     override suspend fun getTasks(forceUpdate: Boolean): Result<List<Task>> {
-        TODO("Not yet implemented")
+        return Result.Success(tasksServiceData.values.toList())
     }
 
     override suspend fun refreshTasks() {
-        observableTasks.value = Result.Success(tasksServiceData.values.toList())
+        observableTasks.value = getTasks()
     }
 
     override fun observeTasks(): LiveData<Result<List<Task>>> {
-        TODO("Not yet implemented")
+        runBlocking { refreshTasks() }
+        return observableTasks
     }
 
     override suspend fun refreshTask(taskId: String) {
-        TODO("Not yet implemented")
+        refreshTasks()
     }
 
     override fun observeTask(taskId: String): LiveData<Result<Task>> {
-        TODO("Not yet implemented")
+        runBlocking { refreshTasks() }
+        return observableTasks.map { tasks ->
+            when (tasks) {
+                is Result.Loading -> Result.Loading
+                is Result.Error -> Result.Error(tasks.exception)
+                is Result.Success -> {
+                    val task = tasks.data.firstOrNull { it.id == taskId }
+                        ?: return@map Result.Error(Exception("Not found"))
+                    Result.Success(task)
+                }
+            }
+        }
     }
 
     override suspend fun getTask(taskId: String, forceUpdate: Boolean): Result<Task> {
-        TODO("Not yet implemented")
+        tasksServiceData[taskId]?.let {
+            return Result.Success(it)
+        }
+        return Result.Error(Exception("Could not find task"))
     }
 
     override suspend fun saveTask(task: Task) {
-        TODO("Not yet implemented")
+        tasksServiceData[task.id] = task
     }
 
     override suspend fun completeTask(task: Task) {
-        TODO("Not yet implemented")
+        val completedTask = task.copy(isCompleted = true)
+        tasksServiceData[task.id] = completedTask
+        refreshTasks()
     }
 
     override suspend fun completeTask(taskId: String) {
-        TODO("Not yet implemented")
+        // Not required for the remote data source.
+        throw NotImplementedError()
     }
 
     override suspend fun activateTask(task: Task) {
-        TODO("Not yet implemented")
+        val activeTask = task.copy(isCompleted = false)
+        tasksServiceData[task.id] = activeTask
+        refreshTasks()
     }
 
     override suspend fun activateTask(taskId: String) {
-        TODO("Not yet implemented")
+        throw NotImplementedError()
     }
 
     override suspend fun clearCompletedTasks() {
-        TODO("Not yet implemented")
+        tasksServiceData = tasksServiceData.filterValues {
+            !it.isCompleted
+        } as LinkedHashMap<String, Task>
     }
 
     override suspend fun deleteAllTasks() {
-        TODO("Not yet implemented")
+        tasksServiceData.clear()
+        refreshTasks()
     }
 
     override suspend fun deleteTask(taskId: String) {
-        TODO("Not yet implemented")
+        tasksServiceData.remove(taskId)
+        refreshTasks()
     }
 
     fun addTasks(vararg tasks: Task) {
--- basic-testing/src/test/java/com/mcmouse88/basic_testing/tasks/TasksViewModelTest.kt
+++ basic-testing/src/test/java/com/mcmouse88/basic_testing/tasks/TasksViewModelTest.kt
@@ -1,21 +1,45 @@
 package com.mcmouse88.basic_testing.tasks
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.mcmouse88.basic_testing.Event
+import com.mcmouse88.basic_testing.R
 import com.mcmouse88.basic_testing.data.Task
 import com.mcmouse88.basic_testing.data.source.FakeTasksRepository
 import com.mcmouse88.basic_testing.utils.getOrAwaitValue
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.TestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runCurrent
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
 import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.CoreMatchers.not
 import org.hamcrest.CoreMatchers.nullValue
 import org.hamcrest.MatcherAssert
+import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
+@OptIn(ExperimentalCoroutinesApi::class)
 class TasksViewModelTest {
 
     @get:Rule
     var executorRule = InstantTaskExecutorRule()
+
+    private val testDispatcher: TestDispatcher = StandardTestDispatcher()
+
+    @Before
+    fun setupDispatcher() {
+        Dispatchers.setMain(testDispatcher)
+    }
+
+    @After
+    fun tearDownDispatcher() {
+        Dispatchers.resetMain()
+    }
 
     private lateinit var tasksRepository: FakeTasksRepository
     private lateinit var tasksViewModel: TasksViewModel
@@ -50,5 +74,23 @@ class TasksViewModelTest {
         // Then the "Add task" action is visible
         val value = tasksViewModel.tasksAddViewVisible.getOrAwaitValue()
         MatcherAssert.assertThat(value, `is`(true))
+    }
+
+    @Test
+    fun `complete task data and snackbar updated`() = runTest {
+        // With a repository that has an active task
+        val task = Task("Title", "Description")
+        tasksRepository.addTasks(task)
+
+        // Complete Task
+        tasksViewModel.completeTask(task, true)
+        runCurrent()
+
+        // Verify the task is completed
+        MatcherAssert.assertThat(tasksRepository.tasksServiceData[task.id]?.isCompleted, `is`(true))
+
+        // The snackbar is updated
+        val snackbarText: Event<Int> = tasksViewModel.snackbarText.getOrAwaitValue()
+        MatcherAssert.assertThat(snackbarText.getContentIfNotHandled(), `is`(R.string.task_marked_complete))
     }
 }
